731d261758dfe270649f6a173abc2a8fa7ea6027
Ten04031977 <ten04031977@mail.ru>
2016-03-10 17:10:57 +0300

    Update am2320.cpp

--- am2320.cpp
+++ am2320.cpp
@@ -1,8 +1,8 @@
 #include "AM2320.h"
 #include <Wire.h>
 // 
-// AM2321 Temperature & Humidity Sensor library for Arduino
-// Сделана Тимофеевым Е.Н. из AM2320-master
+// AM2320 Temperature & Humidity Sensor library for Arduino
+// Сделана Тимофеевым Е.Н.
 
 unsigned int CRC16(byte *ptr, byte length) 
 { 
